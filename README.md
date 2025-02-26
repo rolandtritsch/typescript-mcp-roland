3817238376cacc197466476896f08c14775a6677
Roland Tritsch <roland@tritsch.email>
2025-02-26 13:01:35 +0000

    README: How do make it work/run.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # typescript-mcp-roland
+
 A very simple MCP server to explain who Roland is.
+
+The server is just reading the `about.md` in `files/`.
+
+To make this work, you need to ...
+
+- clone the repository
+- edit the `server.ts` and `server.json`to make sure `serverRoot` is set to the correct path
+- run `npm install` to install the dependencies
+- run `npm run build` to build the TypeScript code
+- configure Windsurf (or any other MCP enabled client) with the snippet in the `server.json` file
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,43 @@
-import { readFile } from "node:fs/promises";
-import { Server } from "@modelcontextprotocol/sdk/server/index.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { readFile } from 'node:fs/promises';
+import { Server } from '@modelcontextprotocol/sdk/server/index.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import {
   CallToolRequestSchema,
   ErrorCode,
   ListToolsRequestSchema,
   McpError,
-} from "@modelcontextprotocol/sdk/types.js";
+} from '@modelcontextprotocol/sdk/types.js';
 
-const server = new Server({
-  name: "roland",
-  version: "1.0.0",
-}, {
-  capabilities: {
-    tools: {}
-  }
-});
+const server = new Server(
+  {
+    name: 'roland',
+    version: '1.0.0',
+  },
+  {
+    capabilities: {
+      tools: {},
+    },
+  },
+);
 
 server.setRequestHandler(ListToolsRequestSchema, async () => {
-  const tools = [{
-    name: "about_roland",
-    description: "Give more information about Roland",
-  }];
+  const tools = [
+    {
+      name: 'about_roland',
+      description: 'Give more information about Roland',
+    },
+  ];
 
   return { tools };
 });
 
+const serverHome = '/home/yoda/Development/Home/typescript-mcp-roland';
 server.setRequestHandler(CallToolRequestSchema, async (request) => {
-  if (request.params.name === "about_roland") {
-    const about = await readFile("files/about.md", "utf-8");
+  if (request.params.name === 'about_roland') {
+    const about = await readFile(`${serverHome}/files/about.md`, 'utf-8');
     return { toolResult: about };
   }
-  throw new McpError(ErrorCode.MethodNotFound, "Method not found");
+  throw new McpError(ErrorCode.MethodNotFound, `Method/Tool not found: ${request.params.name}`);
 });
 
 const transport = new StdioServerTransport();
