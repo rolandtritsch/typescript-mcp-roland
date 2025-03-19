858e87884ee4c6a2dc853a2d08bc42b0cccbe270
Roland Tritsch <roland@tritsch.email>
2025-03-19 16:19:10 +0000

    Update to SDK 1.7.0.

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,16 @@
 import { readFile } from 'node:fs/promises';
-import { Server } from '@modelcontextprotocol/sdk/server/index.js';
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
-import {
-  CallToolRequestSchema,
-  ErrorCode,
-  ListToolsRequestSchema,
-  McpError,
-} from '@modelcontextprotocol/sdk/types.js';
 
-const server = new Server(
-  {
-    name: 'roland',
-    version: '1.0.0',
-  },
-  {
-    capabilities: {
-      tools: {},
-    },
-  },
-);
-
-server.setRequestHandler(ListToolsRequestSchema, async () => {
-  const tools = [
-    {
-      name: 'about_roland',
-      description: 'Give more information about Roland',
-    },
-  ];
-
-  return { tools };
+const server = new McpServer({
+  name: 'roland',
+  version: '0.2.0',
 });
 
 const serverHome = '/home/yoda/Development/Home/typescript-mcp-roland';
-server.setRequestHandler(CallToolRequestSchema, async (request) => {
-  if (request.params.name === 'about_roland') {
-    const about = await readFile(`${serverHome}/files/about.md`, 'utf-8');
-    return { toolResult: about };
-  }
-  throw new McpError(ErrorCode.MethodNotFound, `Method/Tool not found: ${request.params.name}`);
+server.tool('about-roland', 'Give more information about Roland', async () => {
+  const about = await readFile(`${serverHome}/files/about.md`, 'utf-8');
+  return { content: [{ type: 'text', text: about }] };
 });
 
 const transport = new StdioServerTransport();
